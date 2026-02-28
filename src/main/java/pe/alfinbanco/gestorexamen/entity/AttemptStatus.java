21faf10dccb633a12838fc1c75b1313a3d22ba28
PykeMG <joseandree.j42@gmail.com>
2026-02-27 23:38:01 -0500

    se modifica el front end

--- src/main/java/pe/alfinbanco/gestorexamen/entity/AttemptStatus.java
+++ src/main/java/pe/alfinbanco/gestorexamen/entity/AttemptStatus.java
@@ -1,5 +1,13 @@
 package pe.alfinbanco.gestorexamen.entity;
 
 public enum AttemptStatus {
-    IN_PROGRESS, FINISHED, CANCELED
+    IN_PROGRESS, FINISHED, CANCELED;
+
+    public String getDisplayName() {
+        return switch (this) {
+            case IN_PROGRESS -> "en progreso";
+            case FINISHED -> "terminado";
+            case CANCELED -> "cancelado";
+        };
+    }
 }
